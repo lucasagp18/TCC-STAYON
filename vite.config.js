8ddb4de9d5dcd3d10913064082b6c06186074ee7
lucasagp <107081584+lucasagp18@users.noreply.github.com>
2025-09-08 13:39:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,3 +10,11 @@ export default defineConfig({
     tailwindcss()
   ],
 })
+
+export default {
+  server: {
+    allowedHosts: [
+      "e055bdef-2055-469d-af90-9852b2d6f2e8-00-1kw2ty4lfzrg1.picard.replit.dev"
+    ]
+  }
+}
